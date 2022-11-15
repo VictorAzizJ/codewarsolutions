5419d917b5589af70d5296fa53582f15ed3a3b9d
WholesomeAhki <Victor.D.Jackson@gmail.com>
2022-11-15 17:38:46 -0500

    simple boolean to string

---
+++ booleanStringConvert.js
@@ -0,0 +1,14 @@
+/*Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given. */
+function booleanToString(b){
+    //your code here
+   if (b == true){
+     let str = 'true'
+     return str
+   } else{
+     let str = 'false'
+     return str
+   }
+   
+  }
