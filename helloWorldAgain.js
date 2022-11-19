2495455c19394efd7bdbc636e8d695f251dc0a0b
WholesomeAhki <Victor.D.Jackson@gmail.com>
2022-11-18 23:55:55 -0500

    remebering first steps

---
+++ helloWorldAgain.js
@@ -0,0 +1,24 @@
+/*f we run this function, This is an example. will be seen on the screen. As you see, console.log() is an useful method. You will use it a lot!
+
+Task
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+In the end, click "Submit" to submit your code pass this kata. */
+function helloWorld(str){
+    let sayit = "Hello World!"
+    console.log(sayit)
+  }
